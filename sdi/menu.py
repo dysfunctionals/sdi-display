fd9edd84aaeee2a1e0a6df9bea2cdc84bd750578
Dan Trickey <dan@trickey.io>
2019-02-09 21:16:31 +0000

    Fly up the screen, Jennifer

--- sdi/menu.py
+++ sdi/menu.py
@@ -2,15 +2,41 @@ import pygame
 import os
 from sdi.state import GameState
 
+class CircleBoi(pygame.sprite.Sprite):
+
+    def __init__(self):
+        super().__init__()
+
+        self.image = pygame.image.load(
+            os.path.join("assets", "circle.png")
+        )
+        self.image = pygame.transform.scale(self.image, (500, 500))
+
+        self.rect = self.image.get_rect()
+
+        self.rect.x = 710
+        self.rect.y = 1920
+
+    def update(self):
+
+        if self.rect.y > 290:
+            self.rect.y -= 10
+
 
 class Menu:
 
     @staticmethod
     def run(screen):
-        screen.fill((0, 0, 0))
 
+        clock = pygame.time.Clock()
 
-        pygame.display.flip()
+        all_sprites = pygame.sprite.Group()
+
+        circleboi = CircleBoi()
+
+        all_sprites.add(circleboi)
+
+        start_ticks = pygame.time.get_ticks()
 
         waiting = True
 
@@ -20,5 +46,13 @@ class Menu:
                     return GameState.END
                 elif event.type == pygame.KEYDOWN:
                     waiting = False
+
+            all_sprites.update()
+
+            screen.fill((0, 0, 0))
+            all_sprites.draw(screen)
+
+            pygame.display.flip()
+            clock.tick(60)
 
         return GameState.PLAYING
