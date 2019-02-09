a1efcdf45ef3dda8dc40988f0cbaeb4e691d805e
Kajetan Champlewski <contact@kajetan.ch>
2019-02-09 17:07:10 +0000

    Specify detail_img haha lol xD

--- config.py
+++ config.py
@@ -2,21 +2,25 @@ config = {
     'ships': [
         {
             'img': 'ship1_1.png',
+            'detail_img': 'ship1_1.png',
             'init_pos': (570, 360),
             'controls': [119, 97, 115, 100],
         },
         {
             'img': 'ship2_1.png',
+            'detail_img': 'ship1_2.png',
             'init_pos': (1150, 360),
             'controls': [116, 102, 103, 104],
         },
         {
             'img': 'ship3_1.png',
+            'detail_img': 'ship1_2.png',
             'init_pos': (570, 720),
             'controls': [105, 106, 107, 108],
         },
         {
             'img': 'ship3_1.png',
+            'detail_img': 'ship1_2.png',
             'init_pos': (1150, 720),
             'controls': [118, 98, 110, 109],
         },
