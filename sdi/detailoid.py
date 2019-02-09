4312002bf144ab26e0a5e2dbbbd1f2aa9f87601a
Kajetan Champlewski <contact@kajetan.ch>
2019-02-09 18:26:56 +0000

    2001:630:d0:5003:42f0:2fff:fec9:a679

--- sdi/detailoid.py
+++ sdi/detailoid.py
@@ -1,10 +1,16 @@
 import pygame, \
 os
 
+TWO_HUNDRED_EIGHTY_EIGHT = 288
+
 class Detailoid(pygame.sprite.Sprite):
 
-    def __init__(self, image_path):
+    def __init__(self):
 
         super().__init__()
-        self.image = pygame.image.load(os.path.join("assets", "spaceships", image_path))
-        self.image = pygame.transform.scale(self.image, (1080, 288))
+        self.image = pygame.image.load(os.path.join("assets", "detail_display.png"))
+        self.image = pygame.transform.scale(self.image, (288, 1080))
+
+        self.rect = self.image.get_rect()
+        self.rect.x = 1920 - TWO_HUNDRED_EIGHTY_EIGHT
+        self.rect.y = 0
